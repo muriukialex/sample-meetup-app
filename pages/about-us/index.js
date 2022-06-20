caf7a9adf7a0fcc3a51ccb5eca500f13197152b3
alexmuriuki <alexmuriukimaina254@gmail.com>
2022-06-20 22:42:19 +0300

    content for the /about page

--- pages/about-us/index.js
+++ pages/about-us/index.js
@@ -3,7 +3,8 @@ import styles from '../../styles/main.module.css'
 const AboutUsPage = () => {
 	return (
 		<div className={styles.main}>
-			<h1>About page</h1> <p>Create some about content here</p>
+			<h1>About us</h1> <h2>Join a community of engineers and grow</h2>
+			<p>This is an application designed to help event organizers list meetups in several regions around the World</p>
 		</div>
 	)
 }
